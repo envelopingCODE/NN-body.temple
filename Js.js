ff9620fafb48e91aef6db24a99a6367844f2887b
= envelopingCODE <>
2025-01-29 15:46:42 +0100

    TSU

--- Js.js
+++ Js.js
@@ -6,8 +6,8 @@
 // - Individual times: "15, 16:30, 17"
 // - Mix both: "15-17, 18:30"
 const AVAILABLE_SLOTS = [
-  "27.01 15-19",            // One specific time + range
-  "28.01 15-19"             // Range + specific time
+  "29.01 15-19",            // One specific time + range
+  "30.01 15-19"             // Range + specific time
 ];
 
 // ===== END OF CONFIGURATION =====
